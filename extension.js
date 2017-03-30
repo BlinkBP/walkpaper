9aa6032620abbda43da526dc49b12dfcf36bd38c
BlinkBP <jan.kominczyk@gmail.com>
2017-03-30 15:21:10 +0200

    Initial commit

---
+++ extension.js
@@ -0,0 +1,38 @@
+// -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
+// Sample extension code, makes clicking on the panel show a message
+const Gio = imports.gi.Gio;
+const St = imports.gi.St;
+const Meta = imports.gi.Meta
+const Mainloop = imports.mainloop;
+const Main = imports.ui.main;
+
+const WORKSPACE_SCHEMA = 'org.gnome.desktop.wm.preferences';
+const WALLPAPER_KEY = 'workspace-wallpapers';
+const BACKGROUND_SCHEMA = 'org.gnome.desktop.background'
+const CURRENT_WALLPAPER_KEY = 'picture-uri'
+
+function _changeWallpaper() {
+    let pathSettings = new Gio.Settings({ schema_id: WORKSPACE_SCHEMA });
+    let paths = pathSettings.get_strv(WALLPAPER_KEY);
+    let backgroundSettings = new Gio.Settings({ schema_id: BACKGROUND_SCHEMA });
+    let index = global.screen.get_active_workspace_index();
+    let wallpaper = 'file://'.concat(paths[index]);
+    backgroundSettings.set_string(CURRENT_WALLPAPER_KEY, wallpaper);
+}
+
+function init(metadata) {
+  log("Walkpaper initiated.")
+}
+
+let signalId;
+
+function enable() {
+    signalId = global.screen.connect('workspace-switched', _changeWallpaper);
+}
+
+function disable() {
+    if (signalId) {
+	     global.screen.disconnect(signalId);
+	     signalId = 0;
+    }
+}
---
+++ prefs.js
@@ -0,0 +1,189 @@
+// -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
+
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Gettext = imports.gettext.domain('gnome-shell-extensions');
+const _ = Gettext.gettext;
+const N_ = function(e) { return e };
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+const WORKSPACE_SCHEMA = 'org.gnome.shell.extensions.walkpaper';
+const WALLPAPER_KEY = 'workspace-wallpapers';
+
+const WalkpaperModel = new GObject.Class({
+    Name: 'Walkpaper.WalkpaperModel',
+    GTypeName: 'WalkpaperModel',
+    Extends: Gtk.ListStore,
+
+    Columns: {
+        PATH: 0,
+    },
+
+    _init: function(params) {
+        this.parent(params);
+        this.set_column_types([GObject.TYPE_STRING, GObject.TYPE_STRING]);
+
+        this._settings = Convenience.getSettings();
+
+        this._reloadFromSettings();
+
+        // overriding class closure doesn't work, because GtkTreeModel
+        // plays tricks with marshallers and class closures
+        this.connect('row-changed', Lang.bind(this, this._onRowChanged));
+        this.connect('row-inserted', Lang.bind(this, this._onRowInserted));
+        this.connect('row-deleted', Lang.bind(this, this._onRowDeleted));
+    },
+
+    _reloadFromSettings: function() {
+        if (this._preventChanges)
+            return;
+        this._preventChanges = true;
+
+        let newPaths = this._settings.get_strv(WALLPAPER_KEY);
+
+        let i = 0;
+        let [ok, iter] = this.get_iter_first();
+        while (ok && i < newPaths.length) {
+            this.set(iter, [this.Columns.PATH], [newPaths[i]]);
+            ok = this.iter_next(iter);
+            i++;
+        }
+
+         while (ok)
+            ok = this.remove(iter);
+        //Adding new rows
+        for ( ; i < newNames.length; i++) {
+            iter = this.append();
+            this.set(iter, [this.Columns.PATH], [newPaths[i]]);
+        }
+
+        this._preventChanges = false;
+    },
+
+    _onRowChanged: function(self, path, iter) {
+        if (this._preventChanges)
+            return;
+        this._preventChanges = true;
+
+        let index = path.get_indices()[0];
+        let paths = this._settings.get_strv(WALLPAPER_KEY);
+
+        if (index >= names.length) {
+            // fill with blanks
+            for (let i = names.length; i <= index; i++)
+                paths[i] = '';
+        }
+
+        paths[index] = this.get_value(iter, this.Columns.PATH);
+
+        this._settings.set_strv(WALLPAPER_KEY, paths);
+
+        this._preventChanges = false;
+    },
+
+    _onRowInserted: function(self, path, iter) {
+        if (this._preventChanges)
+            return;
+        this._preventChanges = true;
+
+        let index = path.get_indices()[0];
+        let paths = this._settings.get_strv(WALLPAPER_KEY);
+        let pathValue = this.get_value(iter, this.Columns.PATH) || '';
+        paths.splice(index, 0, pathValue);
+
+        this._settings.set_strv(WALLPAPER_KEY, paths);
+
+        this._preventChanges = false;
+    },
+
+    _onRowDeleted: function(self, path) {
+        if (this._preventChanges)
+            return;
+        this._preventChanges = true;
+
+        let index = path.get_indices()[0];
+        let paths = this._settings.get_strv(WALLPAPER_KEY);
+
+        if (index >= paths.length)
+            return;
+
+        paths.splice(index, 1);
+
+        // compact the array
+        for (let i = paths.length - 1; i >= 0 && !paths[i]; i++)
+            paths.pop();
+
+        this._settings.set_strv(WALLPAPER_KEY, paths);
+
+        this._preventChanges = false;
+    },
+});
+
+const WalkpaperSettingsWidget = new GObject.Class({
+    Name: 'Walkpaper.WalkpaperSettingsWidget',
+    GTypeName: 'WalkpaperSettingsWidget',
+    Extends: Gtk.Grid,
+
+    _init: function(params) {
+        this.parent(params);
+        this.margin = 12;
+        this.orientation = Gtk.Orientation.VERTICAL;
+//TODO add new label Paths
+        this.add(new Gtk.Label({ label: '<b>' + _("Workspace Names") + '</b>',
+                                 use_markup: true, margin_bottom: 6,
+                                 hexpand: true, halign: Gtk.Align.START }));
+
+        let scrolled = new Gtk.ScrolledWindow({ shadow_type: Gtk.ShadowType.IN });
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
+        this.add(scrolled);
+
+        this._store = new WalkpaperModel();
+
+        this._treeView = new Gtk.TreeView({ model: this._store,
+                                            headers_visible: false,
+                                            reorderable: true,
+                                            hexpand: true,
+                                            vexpand: true
+                                          });
+
+        let columnPaths = new Gtk.TreeViewColumn({ title: _("Path") });
+        let rendererPaths = new Gtk.CellRendererText({ editable: true });
+        rendererPaths.connect('edited', Lang.bind(this, this._pathEdited));
+        columnPaths.pack_start(rendererPaths, true);
+        columnPaths.add_attribute(rendererPaths, 'text', this._store.Columns.PATH);
+        this._treeView.append_column(columnPaths);
+
+        scrolled.add(this._treeView);
+    },
+
+    _nameEdited: function(renderer, path, new_text) {
+        let [ok, iter] = this._store.get_iter_from_string(path);
+
+        if (ok)
+            this._store.set(iter, [this._store.Columns.LABEL], [new_text]);
+    },
+
+    _pathEdited: function(renderer, path, new_text) {
+        let [ok, iter] = this._store.get_iter_from_string(path);
+
+        if (ok)
+            this._store.set(iter, [this._store.Columns.PATH], [new_text]);
+    },
+});
+
+function init() {
+    Convenience.initTranslations();
+}
+
+function buildPrefsWidget() {
+    let widget = new WalkpaperSettingsWidget();
+    widget.show_all();
+    return widget;
+}
